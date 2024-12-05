92014ed5fca6fb2d3ab98045e5ee75ea4d6851c1
AhlgrenT <theodor17.ahlgren@gmail.com>
2024-12-06 00:41:30 +0100

    Day 3 solved

--- src/Solution3/AOC3_2.java
+++ src/Solution3/AOC3_2.java
@@ -1,14 +1,14 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class AOC3_1 {
+public class AOC3_2 {
 	
     public static void main(String[] args) {
     	int res = 0;
+    	boolean valid = true;
         try {
             File inputFile = new File("../inputs/input3.txt");
             Scanner scanner = new Scanner(inputFile);
@@ -16,24 +16,22 @@ public class AOC3_1 {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 
-                // Split the line by spaces to get individual numbers as strings
-                //String[] words = line.split("\\s+");
-                
-                String regexp = "mul\\(\\d{1,3},\\s*\\d{1,3}\\)";
-                // Convert the string array to an integer array
+                String regexp = "do\\(\\)|don't\\(\\)|mul\\(\\d{1,3},\\s*\\d{1,3}\\)";
                 Pattern pattern = Pattern.compile(regexp);
 		Matcher matcher = pattern.matcher(line);
 		
-		// Find and print all matches
 		while (matcher.find()) {
-			String j = matcher.group();
-		   	int start = j.indexOf("(") + 1; // Start after '('
-			int end = j.indexOf(")");       // End at ')'
-			// Split the numbers by comma
-			String[] parts = j.substring(start, end).split(",");
-			
-
-			res += Integer.parseInt(parts[0].trim()) * Integer.parseInt(parts[1].trim());
+			String word = matcher.group();
+			if(word.equals("do()")){
+				valid = true;
+			} else if (word.equals("don't()")){
+				valid = false;
+			} else if (valid){
+				int start = word.indexOf("(") + 1; // Start after '('
+				int end = word.indexOf(")");       // End at ')'
+				String[] parts = word.substring(start, end).split(",");
+				res += Integer.parseInt(parts[0].trim()) * Integer.parseInt(parts[1].trim());
+			}
 		}
             }
             
